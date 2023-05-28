83948cbda82d9cbe7b3c483675844644bc21e06f
Jyotishman <imjyotishmank@gmail.com>
2023-05-28 12:21:41 +0530

    ALERT_COMPONENT

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,6 +1,6 @@
 import { useState, useContext } from "react"
 import GithubContext from "../../context/github/GithubContext"
-import { clear } from "@testing-library/user-event/dist/clear"
+import AlertContext from "../../context/alert/AlertContext"
 
 function UserSearch() {
   
@@ -8,12 +8,14 @@ function UserSearch() {
 
   const {users, searchUsers, clearUsers} = useContext(GithubContext)
 
+  const {setAlert} = useContext(AlertContext)
+
   const handleChange = (e) => setText(e.target.value)
   
   const handleSubmit = (e) => {
     e.preventDefault()
     if(text === '') {
-        alert('Please enter something')
+        setAlert('Please enter something', 'error')
     } else {
         searchUsers(text)
        
